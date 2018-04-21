4d757b3f105a103a5ed3be14942e4f5a29774fb4
brucejcw <bruce.jcw@gmail.com>
2018-04-21 16:49:25 +0800

    toTableData

--- app/util/schemaUtil.js
+++ app/util/schemaUtil.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const _ = require('lodash');
+
 const schemaModel = {
   type: 'object',
   properties: {},
@@ -38,11 +40,57 @@ const parseSchema = (array, pModel) => {
   });
 };
 
+const tableData = {
+  children: [],
+};
+
+function parse(model, pItem) {
+  const tableItem = {
+    field: model._field,
+    required: model._required || false,
+    description: model.description || '',
+    type: model.type,
+  };
+  pItem.children.push(tableItem);
+
+  if (model.type === 'object') {
+    loopProperties(model, tableItem);
+  } else if (model.type === 'array') {
+    if (_.isArray(model.items)) {
+      console.log('todo');
+    } else { // object
+      if (model.items.type === 'object') {
+        loopProperties(model.items, tableItem);
+      } else {
+        console.log('todo');
+      }
+    }
+  }
+}
+
+function loopProperties(model, tableItem) {
+  const properties = model.properties;
+  if (properties && Object.keys(properties).length) {
+    tableItem.children = [];
+    properties && Object.keys(properties).forEach(key => {
+      const _model = properties[key];
+      _model._field = key;
+      _model._required = (model.required || []).indexOf(key) !== -1;
+      parse(_model, tableItem);
+    });
+  }
+}
+
 module.exports = {
   toJSONSchema: macacaSchema => {
     const model = { ...schemaModel };
     model.required = macacaSchema.filter(c => c.required).map(c => c.field);
     parseSchema(macacaSchema, model);
     return model;
+  },
+  toTableData: schemaData => {
+    const table = { ...tableData };
+    parse(schemaData, table);
+    return table.children[0].children;
   },
 };
