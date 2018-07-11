3497873bdf486dcf3af5407e36349e9fc04ce825
Yuheng Zhang <zhangyuheng91@gmail.com>
2018-07-11 20:40:59 +0800

    refactor: services

--- app/service/project.js
+++ app/service/project.js
@@ -1,87 +1,35 @@
 'use strict';
 
-const fs = require('fs');
-const _ = require('xutil');
-const path = require('path');
 const Service = require('egg').Service;
 
 class ProjectService extends Service {
 
-  async query() {
-    const selector = this.ctx.app.whiteList ? {
-      where: {
-        [this.app.Sequelize.Op.or]: this.ctx.app.whiteList || [],
-      },
-      raw: true,
-    } : {
-      raw: true,
-    };
-    return await this.ctx.model.Project.findAll(selector);
+  async queryAllProject() {
+    return await this.ctx.model.Project.findAll();
   }
 
-  async queryById(projectId) {
-    return await this.ctx.model.Project.findAll({
-      where: {
-        identifer: projectId,
-      },
-      raw: true,
-    });
+  async queryProjectByUniqId() {
+    return await this.ctx.model.Project.findAll();
   }
 
-  async upsertById(identifer, data) {
-    const projectData = await this.ctx.model.Project.findAll({
-      where: {
-        identifer,
-      },
-      raw: true,
-    });
-
-    if (!projectData || !projectData.length) { // add Project, upate whiteList
-      if (this.ctx.app.whiteList) {
-        this.ctx.app.whiteList.push({
-          identifer,
-        });
-      }
-    }
-
-    await this.ctx.model.Project.upsert({
-      ...data,
-    }, {
-      where: {
-        identifer,
-      },
-    });
-    return await this.asyncMigration();
+  async queryProjectByName() {
+    return await this.ctx.model.Project.findAll();
   }
 
-  async removeById(identifer) {
-    await this.ctx.model.Project.destroy({
-      where: {
-        identifer,
-      },
-    });
-    return await this.asyncMigration();
+  async createProject() {
+    return await this.ctx.model.Project.findAll();
   }
 
-  async asyncMigration() {
-    const res = await this.ctx.model.Project.findAll({
-      where: {
-        [this.app.Sequelize.Op.or]: this.ctx.app.whiteList || [],
-      },
-      raw: true,
-    });
+  async updateProject() {
+    return await this.ctx.model.Project.findAll();
+  }
 
-    if (this.ctx.app.config.dataHubStoreDir) {
-      _.mkdir(this.ctx.app.config.dataHubStoreDir);
-      const distRes = res.map(item => {
-        delete item.id;
-        delete item.created_at;
-        delete item.updated_at;
-        return item;
-      });
-      fs.writeFile(path.resolve(this.ctx.app.config.dataHubStoreDir, 'hub.data'), JSON.stringify(distRes, null, 2));
-    }
-    return res;
+  async deleteProjectByUniqId() {
+    return await this.ctx.model.Project.findAll();
+  }
+
+  async syncDataToLocalFile() {
+    return await this.ctx.model.Project.findAll();
   }
 }
 
