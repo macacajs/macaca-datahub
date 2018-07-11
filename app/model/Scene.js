60550eafaee1215f2362bbc2eb34a5d94e1078b4
Yuheng Zhang <zhangyuheng91@gmail.com>
2018-07-11 20:27:05 +0800

    refactor: model definition

--- app/model/Scene.js
+++ app/model/Scene.js
@@ -1,1 +1,51 @@
-// TODO prepare scene
+'use strict';
+
+module.exports = app => {
+  const {
+    STRING,
+    UUID,
+    UUIDV4,
+    JSON,
+  } = app.Sequelize;
+
+  const Scene = app.model.define('scene', {
+    sceneName: {
+      type: STRING,
+      allowNull: false,
+    },
+    data: {
+      type: JSON,
+      defaultValue: {},
+      allowNull: false,
+    },
+    apiUniqId: {
+      type: STRING,
+      allowNull: false,
+    },
+    uniqId: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+  }, {
+    ...app.config.modelCommonOption,
+    indexes: [
+      {
+        fields: [
+          'sceneName',
+          'apiUniqId',
+        ],
+        unique: true,
+      },
+      {
+        fields: [
+          'uniqId',
+        ],
+        unique: true,
+      },
+    ],
+  });
+
+  return Scene;
+};
--- app/model/Schema.js
+++ app/model/Schema.js
@@ -1,1 +1,51 @@
-// TODO prepare schema
+'use strict';
+
+module.exports = app => {
+  const {
+    STRING,
+    UUID,
+    UUIDV4,
+    JSON,
+  } = app.Sequelize;
+
+  const Schema = app.model.define('schema', {
+    type: {
+      type: STRING,
+      allowNull: false,
+    },
+    data: {
+      type: JSON,
+      allowNull: false,
+      defaultValue: {},
+    },
+    apiUniqId: {
+      type: STRING,
+      allowNull: false,
+    },
+    uniqId: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+  }, {
+    ...app.config.modelCommonOption,
+    indexes: [
+      {
+        fields: [
+          'type',
+          'apiUniqId',
+        ],
+        unique: true,
+      },
+      {
+        fields: [
+          'uniqId',
+        ],
+        unique: true,
+      },
+    ],
+  });
+
+  return Schema;
+};
