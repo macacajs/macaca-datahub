b7edffa9525e3b4bdba3d5bd33f756df753c8e58
Yuheng Zhang <zhangyuheng91@gmail.com>
2018-07-31 19:48:10 +0800

    feat: enable validation

--- app/controller/api/schema.js
+++ app/controller/api/schema.js
@@ -14,10 +14,16 @@ class SchemaController extends Controller {
   async update() {
     const ctx = this.ctx;
     const { type } = ctx.params;
-    const { interfaceUniqId, data } = ctx.request.body;
-    ctx.assertParam({ interfaceUniqId, data });
+    const { interfaceUniqId } = ctx.request.body;
+    ctx.assertParam({ interfaceUniqId });
+    const payload = {};
+    for (const key of [ 'enableSchemaValidate', 'schemaData' ]) {
+      if (typeof ctx.request.body[key] !== 'undefined') {
+        payload[key] = ctx.request.body[key];
+      }
+    }
     const res = await ctx.service.schema.updateSchema({
-      interfaceUniqId, type, data,
+      interfaceUniqId, type, payload,
     });
     ctx.success(res);
   }
